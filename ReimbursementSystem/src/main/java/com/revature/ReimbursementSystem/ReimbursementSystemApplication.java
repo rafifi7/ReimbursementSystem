0bc491cc4ed7e77247ee4792855017e50ca791c9
rafifi7 <rafeeadnan7@gmail.com>
2024-07-29 14:17:51 -0400

    skeleton spring

---
+++ ReimbursementSystem/src/main/java/com/revature/ReimbursementSystem/ReimbursementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.revature.ReimbursementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimbursementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimbursementSystemApplication.class, args);
+	}
+
+}
---
+++ ReimbursementSystem/src/test/java/com/revature/ReimbursementSystem/ReimbursementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ReimbursementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
